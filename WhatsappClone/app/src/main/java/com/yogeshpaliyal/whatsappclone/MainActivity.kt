21a27badc97beb8f4d9d6724645de0f3152020ee
Yogesh Choudhary Paliyal <paliyalyogesh@gmail.com>
2021-09-25 17:58:08 +0530

    Create Whatsapp clone sample project

---
+++ WhatsappClone/app/src/main/java/com/yogeshpaliyal/whatsappclone/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.yogeshpaliyal.whatsappclone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.yogeshpaliyal.whatsappclone.ui.theme.WhatsappCloneTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WhatsappCloneTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WhatsappCloneTheme {
+        Greeting("Android")
+    }
+}
